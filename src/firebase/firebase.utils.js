1cad0985c8ffeb2104e353448008a1735c15b8f6
savinps <savinps@gmail.com>
2019-12-25 20:39:43 -0500

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCJLqfGDfI6z9vgWbxoEq9lfPZ5QXxbzEU",
-    authDomain: "ecom-db-9a067.firebaseapp.com",
-    databaseURL: "https://ecom-db-9a067.firebaseio.com",
-    projectId: "ecom-db-9a067",
-    storageBucket: "ecom-db-9a067.appspot.com",
-    messagingSenderId: "280736766357",
-    appId: "1:280736766357:web:590afd7ca261fb95af3e9c",
-    measurementId: "G-Y9KGBDK60F"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(config);
